fa4eaa36b1dad427b689943d0eb3e9269e78a74f
marcelosperalta <marcelosperalta@gmail.com>
2020-11-03 12:55:15 +0100

    rocketseat

--- clone-tesla/src/App.tsx
+++ clone-tesla/src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Page from "./components/Page";
 import { GlobalStyles } from './styles/GlobalStyles';
 
 function App() {
---
+++ clone-tesla/src/components/Model/ModelsWrapper/index.tsx
@@ -0,0 +1,13 @@
+import React, { useRef } from 'react';
+
+import { Container } from './styles';
+
+const ModelsWrapper: React.FC = ({ children }) => {
+  const wrapperRef = useRef<HTMLDivElement>(null)
+
+  return (
+    <Container ref={wrapperRef}>{children}</Container>
+  );
+};
+
+export default ModelsWrapper;
